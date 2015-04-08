27a6a8b6746819f6c95e9857d7d91ffb3b1d8a63
Chuang HsuanChih <hcc@email.com>
2015-04-09 00:55:13 +0800

    Initial Commit

---
+++ CCRangeSliderDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CCRangeSliderDemo
+//
+//  Created by Chuang HsuanChih on 4/9/15.
+//  Copyright (c) 2015 Hsuan-Chih Chuang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CCRangeSliderDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CCRangeSliderDemo
+//
+//  Created by Chuang HsuanChih on 4/9/15.
+//  Copyright (c) 2015 Hsuan-Chih Chuang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
